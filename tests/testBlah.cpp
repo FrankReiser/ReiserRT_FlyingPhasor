127ea10f5ecafc209099f64e9e6f48e9f78a025d
FrankReiser <frank@thereisers.com>
2022-01-08 10:18:32 -0500

    Encapsulate Test Code
    Encapsulate Phase Purity and Mag Purity test code into separate classes so state can be pulled easily after the fact from multiple instances. Golden vs Test  Case.

--- tests/testBlah.cpp
+++ tests/testBlah.cpp
@@ -47,20 +47,12 @@ public:
         }
     }
 
+    void reset() { mean=0.0; M2=0.0; nSamples=0; }
 private:
     long double mean{0.0};
     long double M2{0.0};
     size_t nSamples{0};
 };
-
-double deltaAngle( double angleA, double angleB )
-{
-    auto delta = angleB - angleA;
-    if ( delta > M_PI ) delta -= 2*M_PI;
-    else if ( delta < -M_PI ) delta += 2*M_PI;
-//    delta += ( delta > M_PI ) ? -2*M_PI : ( delta < -M_PI ) ? 2*M_PI : 0;
-    return delta;
-}
 
 bool inTolerance( double value, double desiredValue, double toleranceRatio )
 {
@@ -70,7 +62,151 @@ bool inTolerance( double value, double desiredValue, double toleranceRatio )
     return ( minValue <= value && value <= maxValue );
 }
 
-///@todo Not using all the arguments.
+class PhasePurityAnalyzer
+{
+public:
+
+    static double deltaAngle( double angleA, double angleB )
+    {
+        auto delta = angleB - angleA;
+        if ( delta > M_PI ) delta -= 2*M_PI;
+        else if ( delta < -M_PI ) delta += 2*M_PI;
+        return delta;
+    }
+
+    int analyzeSinusoidPhaseStability(const ComplexToneGenerator::ElementBufferTypePtr & pBuf, size_t nSamples,
+                                      double radiansPerSample, double phi )
+    {
+        int retCode = 0;
+
+        // Reset stats in case an instance is re-run.
+        statsStateMachine.reset();
+
+        for ( size_t n=0; nSamples != n; ++n )
+        {
+            // If n is zero, then we merely want to ensure that our first phase is approximately equivalent
+            // to argument "phi". If not, this is an obvious failure.
+            if ( 0 == n )
+            {
+                // This 1e-9 tolerance check adequate for initial phi setting.
+                if ( !inTolerance( std::arg( pBuf[ n ] ), phi, 1e-9 ) )
+                {
+                    std::cout << "Starting Phase: " <<  std::arg( pBuf[ n ] ) << " Out of Tolerance! Should be: "
+                              << phi << std::endl;
+
+                    retCode = 1;
+                    break;
+                }
+            }
+
+            // We cheat the first sample because there is no previous one in order to compute
+            // a delta. Being that we are expecting a periodic complex waveform, this would be
+            // the radiansPerSample given as an argument.
+            auto phaseDelta = 0 == n ? radiansPerSample : deltaAngle(std::arg(pBuf[n - 1 ] ), std::arg(pBuf[ n ] ) );
+
+            // If the phaseDelta is far too far off of radiansPerSample. This is an obvious failure.
+            // We are trying to catch the oddball here as a few oddballs may not create much variance.
+            // Variance we check later.
+            if ( !inTolerance( phaseDelta, radiansPerSample, 1e-12 ) )
+            {
+                std::cout << "Phase error at sample: " << n << "! Expected: " << radiansPerSample
+                          << ", Detected: " << phaseDelta << std::endl;
+                retCode = 2;
+                break;
+            }
+
+            // Run stats state machine for overall mean and variance check.
+            statsStateMachine.addSample( phaseDelta );
+        }
+
+        // Now test the statistics look good.
+        auto rateStats = statsStateMachine.getStats();
+        if ( !inTolerance( rateStats.first, radiansPerSample, 1e-15 ) )
+        {
+            std::cout << "Mean Angular Rate error! Expected: " << radiansPerSample
+                      << ", Detected: " << rateStats.first << std::endl;
+            retCode = 3;
+        }
+        else if ( rateStats.second > 1.5e-32 )
+        {
+            std::cout << "Phase Noise error! Expected less than: " << 1.5e-32
+                      << ", Detected: " << rateStats.second << std::endl;
+            retCode = 4;
+        }
+
+        std::cout << "Mean Angular Rate: " << rateStats.first << ", Variance: " << rateStats.second << std::endl;
+
+        return retCode;
+    }
+
+private:
+    StatsStateMachine statsStateMachine{};
+};
+
+class MagPurityAnalyzer
+{
+public:
+    int analyzeSinusoidMagnitudeStability(const ComplexToneGenerator::ElementBufferTypePtr & pBuf, size_t nSamples )
+    {
+        int retCode = 0;
+
+        // Reset stats in case an instance is re-run.
+        statsStateMachine.reset();
+
+        for ( size_t n=0; nSamples != n; ++n )
+        {
+            // If the magnitude is far too far off of 1.0. This is an obvious failure.
+            // We are trying to catch the oddball here as a few oddballs may not create much variance.
+            // Variance we check later.
+            auto mag = std::abs( pBuf[ n ] );
+            if ( !inTolerance( mag, 1.0, 1e-15 ) )
+            {
+                std::cout << "Magnitude error at sample: " << n << "! Expected: " << 1.0
+                          << ", Detected: " << mag << std::endl;
+                retCode = 1;
+                break;
+            }
+
+            statsStateMachine.addSample( mag );
+        }
+
+        // Now test the statistics look good.
+        auto magStats = statsStateMachine.getStats();
+        if ( !inTolerance( magStats.first, 1.0, 1e-12 ) )
+        {
+            std::cout << "Mean Magnitude error! Expected: " << 1.0
+                      << ", Detected: " << magStats.first << std::endl;
+            retCode = 2;
+        }
+        else if ( magStats.second > 5e-33 )
+        {
+            std::cout << "Magnitude Noise error! Expected less than: " << 5e-33
+                      << ", Detected: " << magStats.second << std::endl;
+            retCode = 3;
+        }
+
+        double signalPower = magStats.first * magStats.first / 2;   // Should always be 0.5
+        std::cout << "Mean Magnitude: " << magStats.first << ", Variance: " << magStats.second
+                  << ", SNR: " << 10.0 * std::log10( signalPower / magStats.second ) << " dB" << std::endl;
+
+        return retCode;
+    }
+
+private:
+    StatsStateMachine statsStateMachine{};
+};
+
+#if 0
+double deltaAngle( double angleA, double angleB )
+{
+    auto delta = angleB - angleA;
+    if ( delta > M_PI ) delta -= 2*M_PI;
+    else if ( delta < -M_PI ) delta += 2*M_PI;
+//    delta += ( delta > M_PI ) ? -2*M_PI : ( delta < -M_PI ) ? 2*M_PI : 0;
+    return delta;
+}
+
+
 int analyzeSinusoidPhaseStability(const ComplexToneGenerator::ElementBufferTypePtr & pBuf, size_t nSamples,
                                   double radiansPerSample, double phi )
 {
@@ -177,6 +313,7 @@ int analyzeSinusoidMagnitudeStability(const ComplexToneGenerator::ElementBufferT
 
     return retCode;
 }
+#endif
 
 int main( int argc, char * argv[])
 {
@@ -200,6 +337,10 @@ int main( int argc, char * argv[])
     }
 #endif
 
+    // For maximum view of significant digits for diagnostic purposes.
+    std::cout << std::scientific;
+    std::cout.precision(17);
+
     // Instantiate the ComplexToneGen
 //    constexpr double radiansPerSample = M_PI_2;
     double radiansPerSample = cmdLineParser.getRadsPerSample();
@@ -211,8 +352,10 @@ int main( int argc, char * argv[])
     std::unique_ptr< ComplexToneGenerator::ElementType[] > pToneGenSeries{new ComplexToneGenerator::ElementType [ maxSamples] };
     std::unique_ptr< ComplexToneGenerator::ElementType[] > pCmplxExpSeries{new ComplexToneGenerator::ElementType [ maxSamples] };
 
-    std::cout << std::scientific;
-    std::cout.precision(17);
+    PhasePurityAnalyzer toneGenPhasePurityAnalyzer{};
+    PhasePurityAnalyzer cmplxExpPhasePurityAnalyzer{};
+    MagPurityAnalyzer toneGenMagPurityAnalyzer{};
+    MagPurityAnalyzer cmplxExpMagPurityAnalyzer{};
 
     // Ask for some samples from the complex tone generator
     size_t numSamples = 4096;
@@ -224,8 +367,10 @@ int main( int argc, char * argv[])
     std::cout << "New Generator Performance for numSamples: " << numSamples
         << " is " << t1-t0 << " seconds." << std::endl;
 
-    analyzeSinusoidPhaseStability(pToneGenSeries.get(), numSamples, radiansPerSample, phi);
-    analyzeSinusoidMagnitudeStability(pToneGenSeries.get(), numSamples);
+//    analyzeSinusoidPhaseStability(pToneGenSeries.get(), numSamples, radiansPerSample, phi);
+    toneGenPhasePurityAnalyzer.analyzeSinusoidPhaseStability( pToneGenSeries.get(), numSamples, radiansPerSample, phi );
+//    analyzeSinusoidMagnitudeStability(pToneGenSeries.get(), numSamples);
+    toneGenMagPurityAnalyzer.analyzeSinusoidMagnitudeStability( pToneGenSeries.get(), numSamples );
 #if 0
     // What did we get
     for (size_t n = 0; numSamples != n; ++n )
@@ -258,8 +403,10 @@ int main( int argc, char * argv[])
     }
 #endif
 
-    analyzeSinusoidPhaseStability(pCmplxExpSeries.get(), numSamples, radiansPerSample, phi);
-    analyzeSinusoidMagnitudeStability(pCmplxExpSeries.get(), numSamples);
+//    analyzeSinusoidPhaseStability(pCmplxExpSeries.get(), numSamples, radiansPerSample, phi);
+    cmplxExpPhasePurityAnalyzer.analyzeSinusoidPhaseStability( pCmplxExpSeries.get(), numSamples, radiansPerSample, phi );
+//    analyzeSinusoidMagnitudeStability(pCmplxExpSeries.get(), numSamples);
+    cmplxExpMagPurityAnalyzer.analyzeSinusoidMagnitudeStability( pCmplxExpSeries.get(), numSamples );
 
 // New Generator Re-normalizing every 4th sample
 //n: 4095, x: -6.59759965582308316e-02, y: -9.97821210376963585e-01, mag: 1.00000000000000000e+00, phase: -1.63682028109054922e+00
