903c32114e318830329113af7fc14aa233086f39
FrankReiser <frank@thereisers.com>
2022-01-09 13:26:06 -0500

    Added Sundry Programs Stream Output
    Added both a Legacy and a FlyingPhasor executable to stream output for external analysis.

---
+++ sundry/streamLegacyGen.cpp
@@ -0,0 +1,34 @@
+#include "FlyingPhasorToneGenerator.h"
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+//    double radiansPerSample = cmdLineParser.getRadsPerSample();
+//    double phi = cmdLineParser.getPhase();
+    double radiansPerSample = 1.0;
+    double phi = 0.0;
+
+    // Generate Samples
+    constexpr size_t numSamples = 4096;
+    std::unique_ptr< FlyingPhasorToneGenerator::ElementType[] > pToneSeries{new FlyingPhasorToneGenerator::ElementType [ numSamples] };
+    constexpr FlyingPhasorToneGenerator::ElementType j{ 0.0, 1.0 };
+    FlyingPhasorToneGenerator::ElementBufferTypePtr p = pToneSeries.get();
+    for ( size_t n = 0; numSamples != n; ++n )
+    {
+        // Legacy Complex Exponential Equivalent
+        p[n] = exp( j * ( double( n ) * radiansPerSample + phi ) );
+    }
+
+    // Write to standard out. It can be redirected.
+    std::cout << std::scientific;
+    std::cout.precision(17);
+    for ( size_t n = 0; numSamples != n; ++n )
+    {
+        std::cout << p[n].real() << " " << p[n].imag() << std::endl;
+    }
+
+    exit( 0 );
+    return 0;
+}
