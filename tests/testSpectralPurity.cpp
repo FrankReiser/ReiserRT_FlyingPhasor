1d417f474ab8c25808fc2c30d54e402bdbaee280
FrankReiser <frank@thereisers.com>
2022-01-13 18:51:31 -0500

    Abandon Command Line Parsing for Spectral Purity Test

--- tests/testSpectralPurity.cpp
+++ tests/testSpectralPurity.cpp
@@ -1,37 +1,10 @@
 // Created on 20220111
 
 #include "FlyingPhasorToneGenerator.h"
-#include "CommandLineParser.h"
 #include <memory>
 #include <cmath>
 #include <iostream>
 #include <fftw3.h>
-
-using WindowPtrType = std::unique_ptr< FlyingPhasorToneGenerator::PrecisionType[] >;
-
-#if 0
-WindowPtrType blackman( size_t nSamples )
-{
-    WindowPtrType w{new FlyingPhasorToneGenerator::PrecisionType[ nSamples ] };
-
-    for (size_t i = 0; i != nSamples ; ++i )
-    {
-#if 0
-        // 'Symetric'
-        w[i] = 0.42 -
-            0.5 * std::cos( 2.0 * M_PI * double( i ) / double( nSamples - 1) ) +
-            0.08 * std::cos(4.0 * M_PI * double( i ) / double( nSamples - 1 ) );
-#else
-        // 'Periodic'
-        w[i] = 0.42 -
-               0.5 * std::cos( 2.0 * M_PI * double( i ) / double( nSamples ) ) +
-               0.08 * std::cos(4.0 * M_PI * double( i ) / double( nSamples ) );
-#endif
-    }
-
-    return std::move( w );
-}
-#endif
 
 int main( int argc, char * argv[] )
 {
@@ -41,45 +14,14 @@ int main( int argc, char * argv[] )
     std::cout << std::scientific;
     std::cout.precision(17);
 
-    // Parse potential command line. Defaults provided otherwise.
-    CommandLineParser cmdLineParser{};
-    if ( 0 != cmdLineParser.parseCommandLine(argc, argv) )
-    {
-        std::cout << "Failed parsing command line" << std::endl;
-        std::cout << "Optional Arguments are:" << std::endl;
-        std::cout << "\t--radsPerSample=<double>: The radians per sample to used." << std::endl;
-        std::cout << "\t--phase=<double>: The initial phase in radians." << std::endl;
-
-        exit( -1 );
-    }
-#if 1
-    else
-    {
-        std::cout << "Parsed: --radiansPerSample=" << cmdLineParser.getRadsPerSample()
-                  << " --phase=" << cmdLineParser.getPhase() << std::endl << std::endl;
-    }
-#endif
-
-#if 0
-    double radiansPerSample = cmdLineParser.getRadsPerSample();
-    double phi = cmdLineParser.getPhase();
-#else
-//    double radiansPerSample = 1.0;
+    // I am only going to test this with exact basis functions because I want to use
+    // windowless, un-padded FFTs here. It's the most straight forward way at proving
+    // the Flying Phase Generator Noise Floor is comparable to Legacy Methods.
     double radiansPerSample = 3 * (2*M_PI / 4096);  // Third basis function excluding DC.
     double phi = 0.0;
-#endif
 
     // Epoch size
     constexpr size_t numSamples = 4096;
-
-#if 0
-    // Blackman Window of Epoch size
-    auto blackmanW=blackman( numSamples );
-
-    // For maximum view of significant digits for diagnostic purposes.
-    for ( size_t i = 0; i != numSamples; ++i )
-        std::cout << blackmanW[ i ] << std::endl;
-#endif
 
     // Buffers big enough for tone generated and fft output
     std::unique_ptr< FlyingPhasorToneGenerator::ElementType[] > pToneSeries{new FlyingPhasorToneGenerator::ElementType [ numSamples ]  };
@@ -92,14 +34,6 @@ int main( int argc, char * argv[] )
 
     // Generate the tone in the padded buffer.
     pFlyingPhasorToneGen->getSamples( numSamples, pToneSeries.get() );
-
-#if 0 // No Windowing
-    // Multiply numSamples portion of pToneSeries by Blackman window.
-    for ( size_t i=0; numSamples != i; ++i )
-    {
-        pToneSeries[i] *= blackmanW[i];
-    }
-#endif
 
 #if 0
     for ( size_t i=0; numSamples*16 != i; ++i)
