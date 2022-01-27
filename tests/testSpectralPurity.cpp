aeacff3791ac9f3b9463479fed87585b7d7dfd24
FrankReiser <frank@thereisers.com>
2022-01-27 16:14:44 -0500

    Prepping CFAR

--- tests/testSpectralPurity.cpp
+++ tests/testSpectralPurity.cpp
@@ -4,9 +4,137 @@
 #include <memory>
 #include <cmath>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <fftw3.h>
 
-constexpr size_t numSamples = 4096;
+constexpr size_t epochSizePowerTwo = 12;
+constexpr size_t numSamples = 1 << epochSizePowerTwo;
+
+using ComplexBufferType = std::unique_ptr< FlyingPhasorToneGenerator::ElementType[] >;
+using ScalarBufferType = std::unique_ptr< FlyingPhasorToneGenerator::PrecisionType[] >;
+
+struct LocalMax
+{
+    LocalMax( double thePwrLevel, size_t theIndex )
+            : pwrLevel{thePwrLevel}, atIndex{theIndex}
+    {
+    }
+
+    double pwrLevel{};
+    size_t atIndex{};
+};
+
+// Purposefully mis-ordered so that the biggest if first, not last.
+bool operator< ( const LocalMax & a, const LocalMax & b )
+{
+    return a.pwrLevel > b.pwrLevel;
+}
+
+using LocalMaxBuffer = std::vector< LocalMax >;
+
+// Constant False Alarm Rate Algorithm
+// Assumes algorithm will be run on power spectrum of a power of two length ordered as normal
+// fft output would be (positive through negative frequencies with the cusp at the center).
+// It needs to know the power of two because it wraps at DC with twos complement math.
+class CFAR_Algorithm
+{
+public:
+    CFAR_Algorithm( uint32_t powerOfTwo, uint32_t numTrainingCells, uint32_t numGuardCells, double threshold )
+      : numElements( 1 << powerOfTwo )
+      , mask( numElements - 1 )
+      , nTrainingCells( numTrainingCells )
+      , nGuardCells( numGuardCells )
+      , alpha( threshold )
+    {
+    }
+
+    LocalMaxBuffer run( ScalarBufferType & powerSpectrum ) const
+    {
+        LocalMaxBuffer localMaxBuffer{};
+
+        for ( uint32_t cut=0; numElements != cut; ++cut )
+        {
+            double leadingNoisePower{};
+            double trailingNoisePower{};
+
+            auto algIndex = uint32_t( cut - ( nTrainingCells + nGuardCells ) ) & mask;
+
+            for ( uint32_t i = 0; nTrainingCells != i; ++i )
+            {
+#if 0
+                if ( cut == 0 ) std::cout << "\tCUT: " << cut << " Bottom Half Filter algIndex: " << algIndex << std::endl;
+                if ( cut == ( mask >> 1 ) ) std::cout << "\tCUT: " << cut << " Bottom Half Filter algIndex: " << algIndex << std::endl;
+                if ( cut == mask ) std::cout << "\tCUT: " << cut << " Bottom Half Filter algIndex: " << algIndex << std::endl;
+#endif
+                ++algIndex &= mask;
+                leadingNoisePower += powerSpectrum[ algIndex++ ];
+            }
+
+            // Skip over Upper and Lower Guard Cells and the CUT index itself. Well get
+            // to extracting its power when we are ready for it.
+            algIndex += 2 * nGuardCells + 1;
+            algIndex &= mask;
+
+            // Average top half of Training Cells
+            for ( uint32_t i = 0; nTrainingCells != i; ++i )
+            {
+#if 0
+                if ( cut == 0 ) std::cout << "\tCUT: " << cut << " Upper Half Filter algIndex: " << algIndex << std::endl;
+                if ( cut == ( mask >> 1 ) ) std::cout << "\tCUT: " << cut << " Upper Half Filter algIndex: " << algIndex << std::endl;
+                if ( cut == mask ) std::cout << "\tCUT: " << cut << " Upper Half Filter algIndex: " << algIndex << std::endl;
+#endif
+                ++algIndex &= mask;
+                trailingNoisePower += powerSpectrum[ algIndex++ ];
+            }
+
+            // Do we have a Local Maximum (LMX)
+            auto cutPower = powerSpectrum[ cut ];
+            leadingNoisePower /= nTrainingCells;
+            trailingNoisePower /= nTrainingCells;
+            if ( cutPower > alpha * ( leadingNoisePower + trailingNoisePower ) / 2 )
+            {
+//                std::cout << "LMX @" << cut << std::endl;
+                localMaxBuffer.emplace_back( LocalMax{ cutPower, cut } );
+            }
+        }
+
+        // Not here.
+        //std::sort( localMaxBuffer.begin(), localMaxBuffer.end() );
+
+        return std::move( localMaxBuffer );
+    }
+
+private:
+    const uint32_t numElements;
+    const uint32_t mask;
+    const uint32_t nTrainingCells;
+    const uint32_t nGuardCells;
+    const double alpha;
+
+};
+
+ScalarBufferType blackman( size_t nSamples )
+{
+    ScalarBufferType w{new FlyingPhasorToneGenerator::PrecisionType[ nSamples ] };
+
+    for (size_t i = 0; i != nSamples ; ++i )
+    {
+#if 0
+        // 'Symetric'
+        w[i] = 0.42 -
+            0.5 * std::cos( 2.0 * M_PI * double( i ) / double( nSamples - 1) ) +
+            0.08 * std::cos(4.0 * M_PI * double( i ) / double( nSamples - 1 ) );
+#else
+        // 'Periodic'
+        w[i] = 0.42 -
+               0.5 * std::cos( 2.0 * M_PI * double( i ) / double( nSamples ) ) +
+               0.08 * std::cos(4.0 * M_PI * double( i ) / double( nSamples ) );
+#endif
+    }
+
+    return std::move( w );
+}
 
 struct PeakInfo
 {
@@ -56,14 +184,18 @@ int main( int argc, char * argv[] )
     std::cout << std::scientific;
     std::cout.precision(17);
 
-    // Buffers big enough for tone generated and fft output
-    std::unique_ptr< FlyingPhasorToneGenerator::ElementType[] > pToneSeries{new FlyingPhasorToneGenerator::ElementType [ numSamples ] };
-    std::unique_ptr< FlyingPhasorToneGenerator::ElementType[] > pSpectralSeries{new FlyingPhasorToneGenerator::ElementType [ numSamples ] };
+    // Buffers big enough for tone generated and fft output, we are going use x2 zero padding
+    ComplexBufferType pToneSeries{new FlyingPhasorToneGenerator::ElementType [ numSamples * 2 ] };
+    ComplexBufferType pSpectralSeries{new FlyingPhasorToneGenerator::ElementType [ numSamples * 2 ] };
+    ScalarBufferType pPowerSpectrum{new FlyingPhasorToneGenerator::PrecisionType [ numSamples * 2 ] };
+
+    // Blackman Window of Epoch size
+    auto bWnd = blackman( numSamples );
 
     // FFTW wants a plan to execute. It requires the number of samples and the source and
     // destination buffer addresses.
     fftw_plan fftwPlan;
-    fftwPlan = fftw_plan_dft_1d( int(numSamples),
+    fftwPlan = fftw_plan_dft_1d( int(numSamples*2),
                                  (fftw_complex *)pToneSeries.get(),
                                  (fftw_complex *)pSpectralSeries.get(), FFTW_FORWARD, FFTW_ESTIMATE);
 
@@ -72,11 +204,46 @@ int main( int argc, char * argv[] )
     double radiansPerSample = (basisFunctionUnderTest * 2 * M_PI ) / numSamples;
     std::unique_ptr< FlyingPhasorToneGenerator > pFlyingPhasorToneGen{ new FlyingPhasorToneGenerator{ radiansPerSample, 0.0 } };
     pFlyingPhasorToneGen->getSamples( numSamples, pToneSeries.get() );
+#if 1
+    // Multiply Epoch portion of pToneSeries by Blackman window.
+    for ( size_t i=0; numSamples != i; ++i )
+    {
+        pToneSeries[i] *= bWnd[i];
+    }
+#endif
     fftw_execute( fftwPlan );
+#if 1
+    // Create a Power Spectrum from the Complex Magnitude. Note we're x2 expanded here due to zero padding.
+    for ( size_t i=0; numSamples * 2 != i; ++i )
+    {
+        auto mag = std::abs( pSpectralSeries[i] );
+        pPowerSpectrum[i] = mag * mag;
+    }
+
+#if 0
+    for ( size_t i=8; 24 != i; ++i )
+        std::cout << "Power Spec[" << i << "] = " << pPowerSpectrum[i] << std::endl;
+#endif
+
+#else
     auto peaks = findPeaks( pSpectralSeries.get() );
     std::cout << "Top Max of: " << peaks.first.value << " found at index: " << peaks.first.index << std::endl;
     std::cout << "Second Max of: " << peaks.second.value << " found at index: " << peaks.second.index << std::endl;
     std::cout << "SNR: " << 10 * std::log10( peaks.first.value / peaks.second.value ) << " dB" << std::endl;
+#endif
+
+    // nT:5 nG:2 Thresh:5.0 works damned well.
+//    CFAR_Algorithm cfarAlgorithm{ epochSizePowerTwo+1, 5, 2, 5.2 };
+//    CFAR_Algorithm cfarAlgorithm{ epochSizePowerTwo+1, 5, 2, 5.0 };
+    CFAR_Algorithm cfarAlgorithm{ epochSizePowerTwo+1, 5, 2, 4.8 };
+//    CFAR_Algorithm cfarAlgorithm{ epochSizePowerTwo+1, 5, 2, 4.0 };
+    auto lmxTable = std::move( cfarAlgorithm.run( pPowerSpectrum ) );
+    for ( auto & lmx : lmxTable )
+    {
+        std::cout << "LMX @" << lmx.atIndex << ", powerLvl: " << lmx.pwrLevel << std::endl;
+    }
+
+#if 0
 
     // Test case #2
     basisFunctionUnderTest = -8;
@@ -99,6 +266,7 @@ int main( int argc, char * argv[] )
     std::cout << "Top Max of: " << peaks.first.value << " found at index: " << peaks.first.index << std::endl;
     std::cout << "Second Max of: " << peaks.second.value << " found at index: " << peaks.second.index << std::endl;
     std::cout << "SNR: " << 10 * std::log10( peaks.first.value / peaks.second.value ) << " dB" << std::endl;
+#endif
 
     // Done with the plan for now.
     fftw_destroy_plan( fftwPlan );
