7e865113ab2ae488a3ab8810ee974922e88af63c
Frank Reiser <Frank@TheReisers.com>
2023-11-12 15:54:31 -0500

    Streaming Utility Modifications
    We desired binary output data in various precision, chunks size and such.

--- sundry/streamFlyingPhasorGen.cpp
+++ sundry/streamFlyingPhasorGen.cpp
@@ -9,43 +9,141 @@
 
 using namespace ReiserRT::Signal;
 
+void printHelpScreen()
+{
+    std::cout << "Usage:" << std::endl;
+    std::cout << "    streamFlyingPhasorGen [options]" << std::endl;
+    std::cout << "Available Options:" << std::endl;
+    std::cout << "    --help" << std::endl;
+    std::cout << "        Displays this help screen and exits." << std::endl;
+    std::cout << "    --radsPerSample=<double>" << std::endl;
+    std::cout << "        The number of radians per sample to be generated." << std::endl;
+    std::cout << "        Defaults to pi/256 radians per sample if unspecified." << std::endl;
+    std::cout << "    --phase=<double>" << std::endl;
+    std::cout << "        The initial phase of the starting sample in radians." << std::endl;
+    std::cout << "        Defaults to 0.0 radians if unspecified." << std::endl;
+    std::cout << "    --chunkSize=<uint>" << std::endl;
+    std::cout << "        The number of samples to produce per chunk. If zero, no samples are produced." << std::endl;
+    std::cout << "        Defaults to 4096 radians if unspecified." << std::endl;
+    std::cout << "    --numChunks=<uint>" << std::endl;
+    std::cout << "        The number of chunks to generate. If zero, runs continually." << std::endl;
+    std::cout << "        Defaults to 1 radians if unspecified." << std::endl;
+    std::cout << "    --streamFormat=<string>" << std::endl;
+    std::cout << "        t32 - Outputs samples in text format with floating point precision of (9 decimal places)." << std::endl;
+    std::cout << "        t64 - Outputs samples in text format with floating point precision (17 decimal places)." << std::endl;
+    std::cout << "        b32 - Outputs data in raw binary with 32bit precision (uint32 and float), native endian-ness." << std::endl;
+    std::cout << "        b64 - Outputs data in raw binary 64bit precision (uint64 and double), native endian-ness." << std::endl;
+    std::cout << "        Defaults to t64 if unspecified." << std::endl;
+    std::cout << "    --includeX" << std::endl;
+    std::cout << "        Include sample count in the output stream. This is useful for gnuplot using any format" << std::endl;
+    std::cout << "        Defaults to no inclusion if unspecified." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Error Returns:" << std::endl;
+    std::cout << "    1 - Command Line Parsing Error - Unrecognized Long Option." << std::endl;
+    std::cout << "    2 - Command Line Parsing Error - Unrecognized Short Option (none supported)." << std::endl;
+    std::cout << "    3 - Invalid Stream Format Specified" << std::endl;
+}
+
 int main( int argc, char * argv[] )
 {
     // Parse potential command line. Defaults provided otherwise.
     CommandLineParser cmdLineParser{};
-    if ( 0 != cmdLineParser.parseCommandLine(argc, argv) )
-    {
-        std::cout << "Failed parsing command line" << std::endl;
-        std::cout << "Optional Arguments are:" << std::endl;
-        std::cout << "\t--radsPerSample=<double>: The radians per sample to used." << std::endl;
-        std::cout << "\t--phase=<double>: The initial phase in radians." << std::endl;
 
-        exit( -1 );
-    }
-#if 0
-    else
+    auto parseRes = cmdLineParser.parseCommandLine(argc, argv);
+    if ( 0 != parseRes )
     {
-        std::cout << "Parsed: --radiansPerSample=" << cmdLineParser.getRadsPerSample()
-                  << " --phase=" << cmdLineParser.getPhase() << std::endl << std::endl;
+        std::cout << "streamFlyingPhasorGen Parse Error: Use command line argument --help for instructions" << std::endl;
+        exit(parseRes);
     }
-#endif
-    double radiansPerSample = cmdLineParser.getRadsPerSample();
-    double phi = cmdLineParser.getPhase();
 
-    // Generate Samples
-    std::unique_ptr< FlyingPhasorToneGenerator > pFlyingPhasorToneGen{ new FlyingPhasorToneGenerator{ radiansPerSample, phi } };
-    constexpr size_t numSamples = 4096;
-    std::unique_ptr< FlyingPhasorElementType[] > pToneSeries{new FlyingPhasorElementType [ numSamples] };
-    constexpr FlyingPhasorElementType j{0.0, 1.0 };
+    if ( cmdLineParser.getHelpFlag() )
+    {
+        printHelpScreen();
+        exit( 0 );
+    }
+
+    // If one of the text formats, set output precision appropriately
+    auto streamFormat = cmdLineParser.getStreamFormat();
+    if ( CommandLineParser::StreamFormat::Invalid == streamFormat )
+    {
+        std::cout << "streamFlyingPhasorGen Error: Invalid Stream Format Specified. Use --help for instructions" << std::endl;
+        exit( 3 );
+    }
+
+    // Get Frequency and Starting Phase
+    auto radiansPerSample = cmdLineParser.getRadsPerSample();
+    auto phi = cmdLineParser.getPhase();
+
+    // Get Chunk Size and Number of Chunks.
+    const auto chunkSize = cmdLineParser.getChunkSize();
+    const auto numChunks = cmdLineParser.getNumChunks();
+
+    // Instantiate a FlyingPhasor
+    FlyingPhasorToneGenerator flyingPhasorToneGenerator{ radiansPerSample, phi };
+
+    // Allocate Memory for Chunk Size
+    std::unique_ptr< FlyingPhasorElementType[] > pToneSeries{new FlyingPhasorElementType [ chunkSize ] };
+
+    // If we are using a text stream format, set the output precision
+    if ( CommandLineParser::StreamFormat::Text32 == streamFormat)
+    {
+        std::cout << std::scientific;
+        std::cout.precision(9);
+    }
+    else if ( CommandLineParser::StreamFormat::Text64 == streamFormat)
+    {
+        std::cout << std::scientific;
+        std::cout.precision(17);
+    }
+
+    // Are we including Sample count in the output?
+    auto includeX = cmdLineParser.getIncludeX();
+
     FlyingPhasorElementBufferTypePtr p = pToneSeries.get();
-    pFlyingPhasorToneGen->getSamples( p, numSamples );
-
-    // Write to standard out. It can be redirected.
-    std::cout << std::scientific;
-    std::cout.precision(17);
-    for ( size_t n = 0; numSamples != n; ++n )
+    size_t sampleCount = 0;
+    for ( size_t chunk = 0; numChunks != chunk; ++chunk )
     {
-        std::cout << p[n].real() << " " << p[n].imag() << std::endl;
+        flyingPhasorToneGenerator.getSamples( p, chunkSize );
+        if ( CommandLineParser::StreamFormat::Text32 == streamFormat ||
+            CommandLineParser::StreamFormat::Text64 == streamFormat )
+        {
+            for ( size_t n = 0; chunkSize != n; ++n )
+            {
+                if ( includeX ) std::cout << sampleCount++ << " ";
+                std::cout << p[n].real() << " " << p[n].imag() << std::endl;
+            }
+        }
+        else if ( CommandLineParser::StreamFormat::Bin32 == streamFormat )
+        {
+            for ( size_t n = 0; chunkSize != n; ++n )
+            {
+                if ( includeX )
+                {
+                    auto sVal = uint32_t( sampleCount++);
+                    std::cout.write(reinterpret_cast< const char * >(&sVal), sizeof( sVal ) );
+                }
+                auto fVal = float( p[n].real() );
+                std::cout.write(reinterpret_cast< const char * >(&fVal), sizeof( fVal ) );
+                fVal = float( p[n].imag() );
+                std::cout.write(reinterpret_cast< const char * >(&fVal), sizeof( fVal ) );
+            }
+        }
+        else if ( CommandLineParser::StreamFormat::Bin64 == streamFormat )
+        {
+            for ( size_t n = 0; chunkSize != n; ++n )
+            {
+                if ( includeX )
+                {
+                    auto sVal = sampleCount++;
+                    std::cout.write(reinterpret_cast< const char * >(&sVal), sizeof( sVal ) );
+                }
+                auto fVal = p[n].real();
+                std::cout.write(reinterpret_cast< const char * >(&fVal), sizeof( fVal ) );
+                fVal = p[n].imag();
+                std::cout.write(reinterpret_cast< const char * >(&fVal), sizeof( fVal ) );
+            }
+        }
+        std::cout.flush();
     }
 
     exit( 0 );
--- testUtilities/CommandLineParser.cpp
+++ testUtilities/CommandLineParser.cpp
@@ -11,19 +11,27 @@ int CommandLineParser::parseCommandLine( int argc, char * argv[] )
 //    int digitOptIndex = 0;
     int retCode = 0;
 
-    enum eOptions { RadsPerSample=1, Phase=2 };
+    enum eOptions { RadsPerSample=1, Phase, ChunkSize, NumChunks, StreamFormat, Help, IncludeX };
 
+    // While options still left to parse
     while (true) {
 //        int thisOptionOptIndex = optind ? optind : 1;
         int optionIndex = 0;
         static struct option longOptions[] = {
                 {"radsPerSample", required_argument, nullptr, RadsPerSample },
                 {"phase", required_argument, nullptr, Phase },
+                {"chunkSize", required_argument, nullptr, ChunkSize },
+                {"numChunks", required_argument, nullptr, NumChunks },
+                {"streamFormatIn", required_argument, nullptr, StreamFormat },
+                {"help", no_argument, nullptr, Help },
+                {"includeX", no_argument, nullptr, IncludeX },
                 {nullptr, 0, nullptr, 0 }
         };
 
         c = getopt_long(argc, argv, "",
                         longOptions, &optionIndex);
+
+        // When getopt_long has completed the parsing of the command line, it returns -1.
         if (c == -1) {
             break;
         }
@@ -31,50 +39,55 @@ int CommandLineParser::parseCommandLine( int argc, char * argv[] )
         switch (c) {
             case RadsPerSample:
                 radsPerSampleIn = std::stod( optarg );
-#if 0
-                std::cout << "The getopt_long call detected the --radsPerSample=" << optarg
-                          << ". Value extracted = " << radsPerSampleIn << "." << std::endl;
-#endif
                 break;
+
             case Phase:
                 phaseIn = std::stod( optarg );
-#if 0
-                std::cout << "The getopt_long call detected the --phase=" << optarg
-                          << ". Value extracted = " << phaseIn << "." << std::endl;
-#endif
                 break;
+
+            case ChunkSize:
+                chunkSizeIn = std::stoul( optarg );
+                break;
+
+            case NumChunks:
+                numChunksIn = std::stoul( optarg );
+                break;
+
+            case StreamFormat:
+            {
+                // This one is more complicated. We either detect a valid string here, or we don't.
+                const std::string streamFormatStr{ optarg };
+                if ( streamFormatStr == "t32" )
+                    streamFormatIn = StreamFormat::Text32;
+                else if ( streamFormatStr == "t64" )
+                    streamFormatIn = StreamFormat::Text64;
+                else if ( streamFormatStr == "b32" )
+                    streamFormatIn = StreamFormat::Bin32;
+                else if ( streamFormatStr == "b64" )
+                    streamFormatIn = StreamFormat::Bin64;
+                else
+                    streamFormatIn = StreamFormat::Invalid;
+                break;
+            }
+
+            case Help:
+                helpFlagIn = true;
+                break;
+
+            case IncludeX:
+                includeX_In = true;
+                break;
+
             case '?':
-                std::cout << "The getopt_long call returned '?'" << std::endl;
-                retCode = -1;
+//                std::cout << "The getopt_long call returned '?'" << std::endl;
+                retCode = 1;
                 break;
             default:
-                std::cout << "The getopt_long call returned character code" << c << std::endl;
-                retCode = -1;
+//                std::cout << "The getopt_long call returned character code" << c << std::endl;
+                retCode = 2;
                 break;
         }
     }
 
     return retCode;
 }
-
-#if 0
-int main( int argc, char * argv[] ) {
-
-    std::cout << "Hello, World!" << " The value of argc is " << argc << std::endl;
-
-    if (1 < argc)
-    {
-        if ( 0 != parseCommandLine(argc, argv) )
-        {
-            std::cout << "Failed parsing command line" << std::endl;
-            return -1;
-        }
-    }
-    else
-    {
-        std::cout << "No program arguments, entering interactive mode" << std::endl;
-    }
-
-    return 0;
-}
-#endif
